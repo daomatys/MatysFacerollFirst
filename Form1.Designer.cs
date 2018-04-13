3bc7b68f3a1e6435e759d0e5a85b42db215d6e8b
daomatys <red41tiger@mail.ru>
2018-04-14 00:12:27 +0700

    Add files

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace MatysApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(238)))), ((int)(((byte)(236)))));
+            this.button1.Location = new System.Drawing.Point(330, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TIRED";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(899, 378);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImage = global::MatysApp.Properties.Resources.Логотип_Battle_for_Azeroth;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(899, 378);
+            this.panel2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(238)))), ((int)(((byte)(236)))));
+            this.button2.Location = new System.Drawing.Point(494, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "MIND";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(238)))), ((int)(((byte)(236)))));
+            this.button3.Location = new System.Drawing.Point(412, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "START";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(238)))), ((int)(((byte)(236)))));
+            this.label1.Location = new System.Drawing.Point(432, 402);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(238)))), ((int)(((byte)(236)))));
+            this.button4.Location = new System.Drawing.Point(330, 395);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "LATER";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.IndianRed;
+            this.label2.Location = new System.Drawing.Point(596, 403);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 6;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label3.Location = new System.Drawing.Point(267, 403);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 7;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(899, 440);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MatysApp
+{
+    public partial class Form1 : Form
+    {
+        const int Wall = 4; //буфер вокруг обозримого поля 
+        const int Cell = 18; //размер клетки
+        const int ISize = 50 + Wall * 2;
+        const int JSize = 21 + Wall * 2;
+        static int[,] a = new int[ISize, JSize];
+        static int MatchTurns, SummaryTurns;
+        static int NullCount, CrossCount;
+        static bool Result = false; //победа
+        static bool IsX = false; //чей ход 
+        static bool Intellect = false; //ai
+
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //TIRED
+        {
+            label2.ResetText();
+            label3.ResetText();
+            panel2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            MatchTurns = 0;
+            SummaryTurns = 0;
+            NullCount = 0;
+            CrossCount = 0;
+            IsX = false;
+            Result = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e) //MIND
+        {
+            if (MatchTurns == 0 && SummaryTurns == 0 )
+            {
+                if (!Intellect)
+                {
+                    Intellect = true;
+                    button2.Text = "AI ON";
+                }
+                else
+                if (Intellect)
+                {
+                    Intellect = false;
+                    button2.Text = "AI OFF";
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //START
+        {
+            InitArray();
+            button3.Visible = false;
+            button4.Visible = false;
+            panel2.Visible = false;
+            label1.Visible = true;
+            label1.Text = "GO!";
+        }
+
+        private void button4_Click(object sender, EventArgs e) //LATER
+        {
+            Application.Exit();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gPanel = panel1.CreateGraphics();
+            ActualSurface(gPanel);
+        }
+
+        static void ActualSurface(Graphics gPanel)
+        {
+            Pen z = new Pen(Color.Gray, 1);
+            Pen r = new Pen(Color.DimGray, 3);
+            Pen x = new Pen(Color.IndianRed, 3);
+            Pen o = new Pen(Color.DarkTurquoise, 3);
+            ActualMarks(gPanel, r, x, o);
+            ActualContainers(gPanel, z);
+        }
+
+        static void ActualMarks(Graphics gPanel, Pen r, Pen x, Pen o)
+        {
+            for (int i = 4; i < ISize - Wall; i++)
+                for (int j = 4; j < JSize - Wall; j++)
+                    switch (a[i, j])
+                    {
+                        case -1:
+                            if (Result)
+                            {
+                                a[i, j] = 3;
+                                MarkX(gPanel, r, i, j);
+                            }
+                            else MarkX(gPanel, x, i, j);
+                            break;
+                        case 0:
+                            if (Result)
+                            {
+                                a[i, j] = 4;
+                                MarkO(gPanel, r, i, j);
+                            }
+                            else MarkO(gPanel, o, i, j);
+                            break;
+                        case 3:
+                            MarkX(gPanel, r, i, j);
+                            break;
+                        case 4:
+                            MarkO(gPanel, r, i, j);
+                            break;
+                    }
+        }
+
+        static void ActualContainers(Graphics gPanel, Pen z)
+        {
+            int c = 3;
+            for (int i = 0; i < ISize - Wall * 2 + 1; i++)
+                for (int j = 0; j < JSize - Wall * 2 + 1; j++)
+                {
+                    gPanel.DrawLine(z, i * Cell - 1, j * Cell - c - 1, i * Cell - 1, j * Cell + c - 1);
+                    gPanel.DrawLine(z, i * Cell - c - 1, j * Cell - 1, i * Cell + c - 1, j * Cell - 1);
+                }
+        }
+
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics gPanel = panel1.CreateGraphics();
+            int i = (e.Location.X - 1) / Cell + Wall;
+            int j = (e.Location.Y - 1) / Cell + Wall;
+            while (a[i, j + 1] == 1)
+                j++;
+            if (a[i, j] == 1)
+            {
+                TurnMY(gPanel, label1, label2, label3, i, j);
+                Thread.Sleep(10);
+                TurnAI(gPanel, label1, label2, label3, i, j);
+            }
+        }
+
+        static void TurnMY(Graphics gPanel, Label label1, Label label2, Label label3, int i, int j)
+        {
+            if (!Result && a[i, j + 1] != 1 && i < ISize - Wall && j < JSize - Wall)
+                AfterTurn(gPanel, label1, label2, label3, i, j);
+        }
+
+        static void TurnAI(Graphics gPanel, Label label1, Label label2, Label label3, int i, int j)
+        {
+            if (!Result && Intellect && a[i, j - 1] == 1)
+            {
+                Random rnd = new Random();
+                i += rnd.Next(-2, 2);
+                while (a[i, j] != 1)
+                    j--;
+                while (a[i, j + 1] == 1)
+                    j++;
+                AfterTurn(gPanel, label1, label2, label3, i, j);
+            }
+        }
+
+        static void Mark(Graphics gPanel, int i, int j)
+        {
+            switch (a[i, j] = IsX ? -1 : 0)
+            {
+                case -1:
+                    Pen x = new Pen(Color.IndianRed, 3);
+                    MarkX(gPanel, x, i, j);
+                    break;
+                case 0:
+                    Pen o = new Pen(Color.DarkTurquoise, 3);
+                    MarkO(gPanel, o, i, j);
+                    break;
+            }
+        }
+
+        static void MarkX(Graphics gPanel, Pen r, int i, int j)
+        {
+            gPanel.DrawLine(r, (i - Wall) * Cell + 1, 1 + (j - Wall) * Cell, (i - Wall) * Cell + 15, 15 + (j - Wall) * Cell);
+            gPanel.DrawLine(r, (i - Wall) * Cell + 15, 1 + (j - Wall) * Cell, (i - Wall) * Cell + 1, 15 + (j - Wall) * Cell);
+        }
+
+        static void MarkO(Graphics gPanel, Pen r, int i, int j)
+        {
+            gPanel.DrawEllipse(r, (i - Wall) * Cell + 1, 1 + (j - Wall) * Cell, 14, 14);
+        }
+
+        static void AfterTurn(Graphics gPanel, Label label1, Label label2, Label label3, int i, int j)
+        {
+            Mark(gPanel, i, j);
+            CheckWin(i, j);
+            MatchTurns++;
+            label1.Text = $"  {MatchTurns}";
+            if (Result)
+            {
+                Salute(gPanel, label2, label3);
+                label1.Text = "NXT";
+            }
+            IsX = !IsX;
+        }
+
+        static void Salute(Graphics gPanel, Label label2, Label label3)
+        {
+            Random rnd = new Random();
+            switch (IsX)
+            {
+                case true:
+                    SaluteCross(gPanel, rnd);
+                    label2.Text = $"X : {++CrossCount}";
+                    break;
+                case false:
+                    SaluteNull(gPanel, rnd);
+                    label3.Text = $"O : {++NullCount}";
+                    break;
+            }
+        }
+
+        static void SaluteCross(Graphics gPanel, Random rnd)
+        {
+            int red, blue, lineToX, lineToY;
+            for (int k = 0; k < 300; k++)
+            {
+                red = rnd.Next(60, 255);
+                blue = rnd.Next(30, 85);
+                Pen w = new Pen(Color.FromArgb(red, k / 120, blue), rnd.Next(6, 17));
+                lineToX = rnd.Next(-600, 1700);
+                lineToY = rnd.Next(0, 1000);
+                gPanel.DrawLine(w, 448, -17, lineToX, lineToY);
+                Thread.Sleep(5);
+            }
+        }
+
+        static void SaluteNull(Graphics gPanel, Random rnd)
+        {
+            int red, blue, circleX, circleY;
+            for (int k = 0; k < 300; k++)
+            {
+                red = rnd.Next(70, 190);
+                blue = rnd.Next(100, 235);
+                Pen v = new Pen(Color.FromArgb(red, 1, blue), rnd.Next(3, 55));
+                circleX = rnd.Next(-100, 999);
+                circleY = rnd.Next(-100, 478);
+                gPanel.DrawEllipse(v, circleX, circleY, k + 10, k + 10);
+                Thread.Sleep(5);
+            }
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics gPanel = panel1.CreateGraphics();
+            if (Result)
+            {
+                AfterSalute(gPanel);
+                panel2.Visible = true;
+                panel2.Visible = false;
+                Result = false;
+                SummaryTurns += MatchTurns;
+                MatchTurns = 0;
+                label1.Text = "GO!";
+            }
+        }
+
+        static void AfterSalute(Graphics gPanel)
+        {
+            Random rnd = new Random();
+            for (int i = 1; i < 40; i++)
+            {
+                Pen u = new Pen(Color.FromArgb(33, 43, 52), i * 5);
+                gPanel.DrawEllipse(u, rnd.Next(-100, 999), rnd.Next(-100, 478), i * 5, i * 5);
+                Thread.Sleep(10);
+                gPanel.DrawEllipse(u, rnd.Next(-100, 999), rnd.Next(-100, 478), i * 5, i * 5);
+                Thread.Sleep(10);
+            }
+        }
+
+
+        static void InitArray()
+        {
+            for (int i = 0; i < ISize; i++)
+                for (int j = 0; j < JSize; j++)
+                    if (j >= JSize - Wall)
+                        a[i, j] = 2;
+                    else a[i, j] = 1;
+        }
+
+        static void CheckWin(int i, int j)
+        {
+            CheckHorizon(a, i, j);
+            CheckVertical(i, j);
+            CheckDiagonalOne(a, i, j);
+            CheckDiagonalTwo(i, j);
+        }
+
+        static void CheckHorizon(int[,] a, int i, int j)
+        {
+            int m = a[i, j];
+            if (a[i, j - 1] == m && m == a[i, j + 1]) // ooo?X?ooo
+            {
+                if (a[i, j - 2] == m && m == a[i, j + 2]) // oo?xXx?oo
+                    Result = true;
+                else
+                {
+                    if (a[i, j - 2] == m && a[i, j - 3] == m) // o??xXx
+                        Result = true;
+                    else
+                        if (m == a[i, j + 2] && m == a[i, j + 3]) // xXx??o
+                        Result = true;
+                }
+            }
+            else
+            {
+                if (a[i, j - 1] == m && a[i, j - 2] == m && a[i, j - 3] == m && a[i, j - 4] == m) // ????X
+                    Result = true;
+                else
+                    if (m == a[i, j + 1] && m == a[i, j + 2] && m == a[i, j + 3] && m == a[i, j + 4]) // X????
+                    Result = true;
+            }
+        }
+
+        static void CheckVertical(int i, int j)
+        {
+            int[,] b = new int [9, 9];
+            for (int p = 0; p < 9; p++)
+                for (int q = 0; q < 9; q++)
+                    b[q, p] = a[i - 4 + p, j - 4 + q];
+            CheckHorizon(b, 4, 4);
+        }
+
+        static void CheckDiagonalOne(int[,] a, int i, int j)
+        {
+            int m = a[i, j];
+            if (a[i - 1, j - 1] == m && m == a[i + 1, j + 1])
+            {
+                if (a[i - 2, j - 2] == m && m == a[i + 2, j + 2])
+                    Result = true;
+                else
+                {
+                    if (a[i - 2, j - 2] == m && a[i - 3, j - 3] == m)
+                        Result = true;
+                    else
+                        if (m == a[i + 2, j + 2] && m == a[i + 3, j + 3])
+                        Result = true;
+                }
+            }
+            else
+            {
+                if (a[i - 1, j - 1] == m && a[i - 2, j - 2] == m && a[i - 3, j - 3] == m && a[i - 4, j - 4] == m)
+                    Result = true;
+                else
+                    if (m == a[i + 1, j + 1] && m == a[i + 2, j + 2] && m == a[i + 3, j + 3] && m == a[i + 4, j + 4])
+                    Result = true;
+            }
+        }
+
+        static void CheckDiagonalTwo(int i, int j)
+        {
+            int[,] b = new int[9, 9];
+            for (int p = 0; p < 9; p++)
+                for (int q = 0; q < 9; q++)
+                    b[8 - p, q] = a[i - 4 + p, j - 4 + q];
+            CheckDiagonalOne(b, 4, 4);
+        }
+    }
+}
